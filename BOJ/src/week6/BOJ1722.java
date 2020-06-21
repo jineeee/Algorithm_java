605864e58325ab20cb13f6e9396e85f3450d8f17
yejin <jinee0717@daum.net>
2020-06-21 23:32:01 +0900

    6/21

--- BOJ/src/week6/BOJ1722.java
+++ BOJ/src/week6/BOJ1722.java
@@ -2,10 +2,17 @@ package week6;
 
 import java.util.*;
 
+/*
+ * 백준 순열의 순서 - 팩토리얼, DP
+ * https://www.acmicpc.net/problem/1722
+ */
+
 public class BOJ1722 {
-	static int n, flag, k=Integer.MAX_VALUE, cnt=0;
-	static int[] arr, result, input;
-	static boolean[] visit;
+	static int n, flag, cnt=0;
+	static long k;
+	static long[] fac = new long[21];
+	static boolean[] visit = new boolean[21];
+ 	static int[] arr, result, input;
 	static boolean print;
 
 	public static void main(String[] args) {
@@ -13,49 +20,38 @@ public class BOJ1722 {
 		n = scanner.nextInt();
 		arr = new int[n];
 		result = new int[n];
-		visit = new boolean[n];
 		flag = scanner.nextInt();
+		
+		fac[0] = 1;
+		for(int i=1; i<=20; i++) //팩토리얼 구하기
+            fac[i] = fac[i-1]*i;
+		
 		if(flag==1) {
-			k = scanner.nextInt();
-			for(int i=1; i<=n; i++) arr[i-1] = i;
-			permu(0);
+			k = scanner.nextLong();
+			for(int i=0; i<n; i++) { // i는 지금까지 확정된 숫자의 개수
+				for(int j=1; j<=n; j++) { // j는 배열의 요소가 될 숫자들
+					if(visit[j]) continue;
+					if(fac[n-i-1]<k) k-= fac[n-i-1];
+					else {
+						arr[i] = j;
+						visit[j] = true;
+						break;
+					}
+				}
+			}
+			for(int i:arr) System.out.print(i + " ");
 		}
 		else {
-			input = new int[n];
-			for(int i=0; i<n; i++) {
-				arr[i] = i+1;
-				input[i] = scanner.nextInt();
-			}
-			permu(0);
-		}
-	}
-	
-	static void permu(int current) {
-		if(cnt>=k || print) return;
-		if(current ==n) {
-			cnt++;
-			if(flag==1) {
-				if(cnt==k) {
-					for(int i:result) System.out.print(i + " ");
+			for(int i=0; i<n; i++) arr[i] = scanner.nextInt();
+			
+			long ans = 1;
+			for(int i=0; i<n; i++) { // 입력 배열의 길이만큼 탐색
+				for(int j=1; j<arr[i]; j++) {
+					if(!visit[j]) ans += fac[n-i-1];
 				}
-			}else if(flag==2){
-				System.out.println(Arrays.toString(result));
-				boolean flag = true;
-				for(int i=0; i<n; i++) if(result[i]!=input[i]) flag=false;
-				if(flag) {
-					System.out.print(cnt);
-					print = true;
-				}
+				visit[arr[i]] = true;
 			}
-		}else {
-			for(int i=0; i<arr.length; i++) {
-				if(!visit[i]) {
-					visit[i] = true;
-					result[current] = arr[i];
-					permu(current+1);
-					visit[i] = false;
-				}
-			}
+			System.out.print(ans);
 		}
 	}
 }
