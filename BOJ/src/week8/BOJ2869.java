afee789d57695e17324396e634af450cdb062927
yejin <jinee0717@daum.net>
2020-08-07 00:04:38 +0900

    8/7

--- BOJ/src/week8/BOJ2869.java
+++ BOJ/src/week8/BOJ2869.java
@@ -3,6 +3,11 @@ package week8;
 import java.io.*;
 import java.util.*;
 
+/*
+ * 달팽이는 올라가고 싶다 - 이진탐색
+ * https://www.acmicpc.net/problem/2869
+ */
+
 public class BOJ2869 {
 
 	public static void main(String[] args) throws IOException {
@@ -15,17 +20,19 @@ public class BOJ2869 {
 		int ans = 0;
 		int s = 0;
 		int e = v;
-		int mid = 0;
-		while(s<=e) {
+		int mid = (s+e)/2;
+		int sub = a-b;
+		
+		while(true) {
+			ans = mid/sub; // 답은 올라간 거리를 a-b로 나눈 값
+			
+			if(v-(sub*ans)>a) s = mid+1; // 거리가 모자란 경우
+			else if(v-(sub*ans)<a) { // 일수를 더 줄일 수 있는 경우
+				if (mid+a-v >= sub) e = mid-1;
+				else break;
+			}else break; // a만큼 올라가면 정상인 경우
 			mid = (s+e)/2;
-			int night = (int) Math.ceil( (double) mid / (a - b));
-			int day = night + b;
-			if(day<v) s = mid+1;
-			else {
-				ans = mid;
-				e = mid-1;
-			}
 		}
-		System.out.print(ans);
+		System.out.print(ans+1); // a만큼 올라간 후 도달하므로 날짜 +1
 	}
 }
