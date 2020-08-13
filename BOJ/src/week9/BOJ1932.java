08107283c05dd4c5fc25312ebdf101799951b0af
yejin <jinee0717@daum.net>
2020-08-14 00:00:15 +0900

    8/14

--- BOJ/src/week9/BOJ1932.java
+++ BOJ/src/week9/BOJ1932.java
@@ -29,7 +29,7 @@ public class BOJ1932 {
 			for(int i=1; i<=N; i++) {
 				col[row+1] = i; // 다음 행, i번째 열에 퀸을 놓음
 				if(isPossible(row+1)) { // 검증이 필요한 행을 매개변수로 보냄
-					dfs(row+1);
+					dfs(row+1); // 다음 행을 매개변수로 재귀 호출
 				}
 			}
 		}
@@ -37,8 +37,8 @@ public class BOJ1932 {
 
 	static boolean isPossible(int row) {
 		for(int i=1; i<row; i++) {
-			if(col[i] == col[row]) return false;
-			if(Math.abs(i-row)==Math.abs(col[i]-col[row])) return false;
+			if(col[i] == col[row]) return false; // 같은 열에 있으면 false
+			if(Math.abs(i-row)==Math.abs(col[i]-col[row])) return false; // 대각선에 위치하면 false
 		}
 		return true;
 	}
