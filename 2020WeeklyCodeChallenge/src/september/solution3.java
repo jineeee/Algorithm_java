9784bd9d3c889490c6533b46c0f6f25c0c5b921e
yejin <jinee0717@daum.net>
2020-09-11 00:04:20 +0900

    9/11

--- 2020WeeklyCodeChallenge/src/september/solution3.java
+++ 2020WeeklyCodeChallenge/src/september/solution3.java
@@ -1,5 +1,7 @@
 package september;
 
+import java.util.*;
+
 public class solution3 {
 	
 	public static void main(String[] args) {
@@ -9,16 +11,29 @@ public class solution3 {
 	
 	static int solution(int[] a) {
         int answer = 0;
+        int[] left_min = new int[1000001];
+        int[] right_min = new int[1000001];
         
         for(int i=0; i<a.length; i++) {
-        	
+        	if(i==0) left_min[i] = a[i];
+        	else left_min[i] = Math.min(left_min[i-1], a[i]);
         }
-        
+        for(int i=a.length-1; i>-1; i--) {
+        	if(i==a.length-1) right_min[i] = a[i];
+        	else right_min[i] = Math.min(right_min[i+1], a[i]);
+        }
+        int cnt = 0;
+        for(int i=0; i<a.length; i++) {
+        	if(i==0) cnt++;
+        	else if(i==a.length-1) cnt++;
+        	else {
+        		int p=0;
+        		if(left_min[i-1] < a[i]) p++;
+        		if(right_min[i-1] < a[i]) p++;
+        		if(p!=2) cnt++;
+        	}
+        }
+        System.out.print(cnt);
         return answer;
     }
-	
-//	static boolean pop(int[] a, int i) {
-//		
-//	}
-
 }
